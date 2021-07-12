d553dbbdf433f35fc3ab14ee9725467532e06726
nahidmk <empiremanik@gmail.com>
2021-07-12 19:20:12 +0600

    frist commit

--- lib/MainPage.dart
+++ lib/MainPage.dart
@@ -1,4 +1,6 @@
+import 'package:cleaningapp/widget/date_time_picker.dart';
 import 'package:cleaningapp/widget/selected_cleaning.dart';
+import 'package:date_format/date_format.dart';
 import 'package:flutter/material.dart';
 import 'package:cleaningapp/constant/ColorScheme.dart';
 import 'package:cleaningapp/CalendarPage.dart';
@@ -24,10 +26,44 @@ class mainPage extends StatefulWidget {
 class _mainPageState extends State<mainPage> {
   String selectedType = "initial";
   String selectedFrequency = "monthly";
+  bool _fridge = false;
+  bool _organise = false;
+  bool _blinds = false;
+  bool _patio = false;
+  bool _grocery = false;
+  bool _curtains = false;
+
+  String _date = '';
+  String _time = '';
+
   @override
   Widget build(BuildContext context) {
     double ScreenHeight = MediaQuery.of(context).size.height;
     double ScreenWidth = MediaQuery.of(context).size.width;
+
+
+    void ShowData(){
+      String data = "Cleaning Type : $selectedType Cleaning\n"
+          "Cleaning Frequency : $selectedFrequency \n"
+          "Extras Cleaning : ${!_blinds?'':"Blinds,"} "
+          "${!_curtains?'':"Curtains,"} "
+          "${!_fridge?'':"Fridge,"} "
+          "${!_organise?'':"Organise,"} "
+          "${!_grocery?'':"Grocery,"} "
+          "${!_patio?'':"Patio,"} \n"
+          "Date : $_date \n"
+          "Time : $_time \n";
+      print('data : $data');
+    }
+
+      void setDateTime(String date, String time){
+        setState(() {
+          _date = date;
+          _time = time;
+        });
+      }
+
+
     return Scaffold(
       backgroundColor: purple,
       appBar: AppBar(
@@ -41,27 +77,26 @@ class _mainPageState extends State<mainPage> {
       ),
       body: Container(
         height: ScreenHeight,
+        width: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.only(top: 10,left: 10,right: 10),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(topLeft: Radius.circular(40), topRight: Radius.circular(40)),
+            color: Colors.white
+        ),
         // decoration: BoxDecoration(border: Border.all(color: Colors.red,width: 2)),
         child: ListView(
           children: [
-                 Container(
-                      width: MediaQuery.of(context).size.width,
-                      height: ScreenHeight,
-                      padding: EdgeInsets.only(top: 10,left: 10,right: 10),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.only(topLeft: Radius.circular(40), topRight: Radius.circular(40)),
-                          color: Colors.white
-                      ),
-                      child: Column(
+
+                      Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          SizedBox(height: 10,),
+                          SizedBox(height: 20,),
                           Text("Selected Cleaning", style: TextStyle(
                               fontSize: 16,
                               fontWeight: FontWeight.w600
                           ),),
 
-                          SizedBox(height: 10,),
+                          SizedBox(height: 20,),
 
                           Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -111,7 +146,7 @@ class _mainPageState extends State<mainPage> {
                               fontSize: 16,
                               fontWeight: FontWeight.w600
                           ),),
-                          SizedBox(height: 10,),
+                          SizedBox(height: 20,),
                           Container(
                             width: MediaQuery.of(context).size.width,
                             child: Row(
@@ -183,38 +218,96 @@ class _mainPageState extends State<mainPage> {
                               ],
                             ),
                           ),
-                          SizedBox(height: 10,),
-                          Text('Selected Extras',style: TextStyle(
+                          SizedBox(height: 20,),
+                          Text('Select Extras',style: TextStyle(
                               fontSize: 16,
                               fontWeight: FontWeight.w600
                           ),),
-                          SizedBox(height: 10,),
+                          SizedBox(height: 20,),
                           Container(
                             height: 90,
                             // decoration: BoxDecoration(border: Border.all(color: Colors.black,width: 2)),
                             child: ListView(
                               scrollDirection: Axis.horizontal,
                               children: [
-                                extraWidget("fridge", "Inside Fridge", true),
+                                GestureDetector(
+                                  onTap: (){
+                                    setState(() {
+                                      _fridge = !_fridge;
+                                    });
+                                  },
+                                  child: extraWidget("fridge", "Inside Fridge", _fridge),
+                                ),
                                 SizedBox(width: 10,),
-                                extraWidget("organise", "Organise", false),
+
+                                GestureDetector(
+                                  onTap: (){
+                                    setState(() {
+                                      _organise = !_organise;
+                                    });
+                                  },
+                                  child: extraWidget("organise", "Organise", _organise),
+                                ),
+
                                 SizedBox(width: 10,),
-                                extraWidget("blind", "Small Blinds", false),
+                                GestureDetector(
+                                  onTap: (){
+                                    setState(() {
+                                      _blinds = !_blinds;
+                                    });
+                                  },
+                                  child: extraWidget("blind", "Small Blinds", _blinds),
+                                ),
+
                                 SizedBox(width: 10,),
-                                extraWidget("garden", "Patio", false),
+                                GestureDetector(
+                                  onTap: (){
+                                    setState(() {
+                                      _patio = !_patio;
+                                    });
+                                  },
+                                  child: extraWidget("garden", "Patio", _patio),
+                                ),
+
                                 SizedBox(width: 10,),
-                                extraWidget("organise", "Grocery", true),
+                                GestureDetector(
+                                  onTap: (){
+                                    setState(() {
+                                      _grocery = !_grocery;
+                                    });
+                                  },
+                                  child: extraWidget("organise", "Grocery", _grocery),
+                                ),
+
                                 SizedBox(width: 10,),
-                                extraWidget("blind", "Curtains", false),
+                                GestureDetector(
+                                  onTap: (){
+                                    setState(() {
+                                      _curtains = !_curtains;
+                                    });
+                                  },
+                                  child: extraWidget("blind", "Curtains", _curtains),
+                                ),
+
                               ],
                             ),
                           ),
 
-                          SizedBox(height: 10,),
+                          SizedBox(height: 20,),
+                          Text('Select Date and time',style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600
+                          ),),
+
+                          SizedBox(height: 20,),
+                          DateTimePicker(setDateTime: setDateTime,),
+
+                          SizedBox(height: 20,),
+
 
                           Center(
                             child: GestureDetector(
-                              onTap: ()=>Navigator.push(context, MaterialPageRoute(builder: (_)=>CalendarPage())),
+                              onTap: ShowData,
                               child: Container(
                                 width: ScreenWidth*0.4,
                                 height: ScreenHeight*0.07,
@@ -224,7 +317,7 @@ class _mainPageState extends State<mainPage> {
 
                                 ),
                                 child: Center(
-                                  child: Text("Next", style: TextStyle(
+                                  child: Text("Save", style: TextStyle(
                                       fontSize: 19,
                                       color: Colors.white,
                                       fontWeight: FontWeight.w600
@@ -235,10 +328,6 @@ class _mainPageState extends State<mainPage> {
                           )
                         ],
                       )
-
-                  ),
-
-
 
           ],
         ),
@@ -310,4 +399,6 @@ class _mainPageState extends State<mainPage> {
     );
   }
 }
+
+
 
---
+++ lib/widget/date_time_picker.dart
@@ -0,0 +1,175 @@
+import 'package:date_format/date_format.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class DateTimePicker extends StatefulWidget {
+
+  final Function setDateTime;
+
+  DateTimePicker({ this.setDateTime});
+
+  @override
+  _DateTimePickerState createState() => _DateTimePickerState();
+}
+
+class _DateTimePickerState extends State<DateTimePicker> {
+  double _height;
+  double _width;
+
+  String _setTime, _setDate;
+
+  String _hour, _minute, _time;
+
+  String dateTime;
+
+  DateTime selectedDate = DateTime.now();
+
+  TimeOfDay selectedTime = TimeOfDay(hour: 00, minute: 00);
+
+  TextEditingController _dateController = TextEditingController();
+  TextEditingController _timeController = TextEditingController();
+
+  Future<Null> _selectDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: selectedDate,
+        initialDatePickerMode: DatePickerMode.day,
+        firstDate: DateTime(2015),
+        lastDate: DateTime(2101));
+    if (picked != null)
+      setState(() {
+        selectedDate = picked;
+        _dateController.text = DateFormat.yMd().format(selectedDate);
+      });
+  }
+
+  Future<Null> _selectTime(BuildContext context) async {
+    final TimeOfDay picked = await showTimePicker(
+      context: context,
+      initialTime: selectedTime,
+    );
+    if (picked != null)
+      setState(() {
+        selectedTime = picked;
+        _hour = selectedTime.hour.toString();
+        _minute = selectedTime.minute.toString();
+        _time = _hour + ' : ' + _minute;
+        _timeController.text = _time;
+        _timeController.text = formatDate(
+            DateTime(2019, 08, 1, selectedTime.hour, selectedTime.minute),
+            [hh, ':', nn, " ", am]).toString();
+      });
+  }
+
+  @override
+  void initState() {
+    // _dateController.text = DateFormat.yMd().format(DateTime.now());
+    _dateController.text = "Choose date";
+
+    // _timeController.text = formatDate(
+    //     DateTime(2019, 08, 1, DateTime.now().hour, DateTime.now().minute),
+    //     [hh, ':', nn, " ", am]).toString();
+
+    _timeController.text = "Choose Time";
+
+    // widget.setDateTime(_dateController.text,_timeController.text);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _height = MediaQuery.of(context).size.height;
+    _width = MediaQuery.of(context).size.width;
+    dateTime = DateFormat.yMd().format(DateTime.now());
+    return  Container(
+        width: _width,
+        // height: _height,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Text(
+                  'Choose Date',
+                  style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: 0.5),
+                ),
+
+                InkWell(
+                  onTap: () {
+                    _selectDate(context);
+                    widget.setDateTime(_dateController.text,_timeController.text);
+                  },
+                  child: Container(
+                    width: _width / 2.5,
+                    height: _height*0.07,
+                    margin: EdgeInsets.only(top: _height*0.01),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(color: Colors.grey[200],borderRadius: BorderRadius.all(Radius.circular(10)),border: Border.all(color: Colors.black.withOpacity(0.3)),),
+                    child: TextFormField(
+                      style: TextStyle(fontSize: 20),
+                      textAlign: TextAlign.center,
+                      enabled: false,
+                      keyboardType: TextInputType.text,
+                      controller: _dateController,
+                      onSaved: (String val) {
+                        _setDate = val;
+                      },
+                      decoration: InputDecoration(
+                          disabledBorder:
+                          UnderlineInputBorder(borderSide: BorderSide.none),
+                          // labelText: 'Time',
+                          // contentPadding: EdgeInsets.only(top: 0.0)
+                        ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                Text(
+                  'Choose Time',
+                  style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: 0.5),
+                ),
+                InkWell(
+                  onTap: () {
+                    _selectTime(context);
+                    widget.setDateTime(_dateController.text,_timeController.text);
+                  },
+                  child: Container(
+                    width: _width / 2.5,
+                    height: _height*0.07,
+                    margin: EdgeInsets.only(top: _height*0.01),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(color: Colors.grey[200],borderRadius: BorderRadius.all(Radius.circular(10)),border: Border.all(color: Colors.black.withOpacity(0.3)),),
+                    child: TextFormField(
+                      style: TextStyle(fontSize: 20),
+                      textAlign: TextAlign.center,
+                      onSaved: (String val) {
+                        _setTime = val;
+                      },
+                      enabled: false,
+                      keyboardType: TextInputType.text,
+                      controller: _timeController,
+                      decoration: InputDecoration(
+                          disabledBorder:
+                          UnderlineInputBorder(borderSide: BorderSide.none),
+                          // labelText: 'Time',
+                          // contentPadding: EdgeInsets.all(5)
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+
+    );
+  }
+}
